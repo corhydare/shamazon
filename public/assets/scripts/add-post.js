beadf56e1c35a6c71a0d7642ed5a6768be55d0fd
Unknown <danila.vpopov@gmail.com>
2022-04-11 21:49:58 -0700

    new things are better

---
+++ public/assets/scripts/add-post.js
@@ -0,0 +1,29 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="create-title"]').value;
+  const post_content = document.querySelector(
+    'input[name="create-content"]'
+  ).value;
+
+  const response = await fetch(`/api/posts`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      post_content,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    document.location.replace("/single-post");
+  }
+};
+
+document
+  .querySelector(".create-form")
+  .addEventListener("submit", newFormHandler);
--- public/assets/scripts/login.js
+++ public/assets/scripts/login.js
@@ -7,7 +7,10 @@ const loginFormHandler = async (event) => {
   if (email && password) {
     const response = await fetch("/api/users/login", {
       method: "POST",
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({
+        email,
+        password,
+      }),
       headers: { "Content-Type": "application/json" },
     });
 
@@ -19,33 +22,6 @@ const loginFormHandler = async (event) => {
   }
 };
 
-//////////////////////
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector("#name-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-
-  if (name && email && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ name, email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
 document
-  .querySelector("#signup-form")
-  .addEventListener("submit", signupFormHandler);
-
-document
-  .querySelector("#login-form")
+  .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
